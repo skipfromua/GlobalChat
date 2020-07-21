cd3a732d457bd00aeed21d3cb0bc198d4372b126
skipfromua <skipfromua@gmail.com>
2020-07-21 23:04:57 +0300

    fix

--- Chat/views.py
+++ Chat/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, redirect
 from .models import Messages
 from django.views import View
+from django.http import JsonResponse
 
 
 class ChatView(View):
-
     def get(self, request):
         messages = Messages.objects.order_by('-pub_date')
+        if request.is_ajax():
+            if messages:
+                message = Messages.objects.latest('pub_date')
+                response = {
+                            'id': message.id,
+                            'text_message': message.text_message,
+                            'pub_date': message.pub_date.strftime("%H:%M:%S")
+                            }
+                return JsonResponse(response)
         context = {
-            'messages': messages
+            'last_message': messages[0].id,
+            'messages': messages,
         }
         return render(request, 'Chat/index.html', context)
 
